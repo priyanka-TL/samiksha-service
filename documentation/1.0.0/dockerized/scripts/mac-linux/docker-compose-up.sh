eb47f38d536aec3aafab72f8e5b7f2392a579d53
praveenKDass <praveendass@shikshalokam.org>
2024-09-26 13:16:03 +0530

    changes:samiksha env added

--- documentation/1.0.0/dockerized/scripts/mac-linux/docker-compose-up.sh
+++ documentation/1.0.0/dockerized/scripts/mac-linux/docker-compose-up.sh
@@ -6,7 +6,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Set environment variables
 export notification_env="$SCRIPT_DIR/notification_env"
 export scheduler_env="$SCRIPT_DIR/scheduler_env"
-export project_env="$SCRIPT_DIR/project_env"
+export samiksha_env="$SCRIPT_DIR/samiksha_env"
 export users_env="$SCRIPT_DIR/user_env"
 export interface_env="$SCRIPT_DIR/interface_env"
 export entity_management_env="$SCRIPT_DIR/entity_management_env"
--- documentation/1.0.0/dockerized/scripts/windows/docker-compose-down.bat
+++ documentation/1.0.0/dockerized/scripts/windows/docker-compose-down.bat
@@ -5,7 +5,7 @@ set "users_env=%cd%\user_env"
 set "interface_env=%cd%\interface_env"
 set "scheduler_env=%cd%\scheduler_env"
 set "notification_env=%cd%\notification_env"
-set "project_env=%cd%\project_env"
+set "samiksha_env=%cd%\samiksha_env"
 set "entity_management_env=%cd%\entity_management_env"
 
 rem Run docker-compose
@@ -16,7 +16,7 @@ set "users_env="
 set "interface_env="
 set "scheduler_env="
 set "notification_env="
-set "project_env="
+set "samiksha_env="
 set "entity_management_env="
 
 pause
