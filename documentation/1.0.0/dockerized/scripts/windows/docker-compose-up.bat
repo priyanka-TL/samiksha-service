b964cbf8d26e21d52065587436a5203640541ffa
praveenKDass <praveendass@shikshalokam.org>
2024-09-26 12:16:22 +0530

    changes:local docker setup

---
+++ documentation/1.0.0/dockerized/scripts/windows/docker-compose-up.bat
@@ -0,0 +1,22 @@
+@echo off
+
+rem Set environment variables
+set "users_env=%cd%\user_env"
+set "interface_env=%cd%\interface_env"
+set "scheduler_env=%cd%\scheduler_env"
+set "notification_env=%cd%\notification_env"
+set "project_env=%cd%\project_env"
+set "entity_management_env=%cd%\entity_management_env"
+
+rem Run docker-compose
+docker-compose -f docker-compose-project.yml up
+
+rem Optionally, clear environment variables after use
+set "users_env="
+set "interface_env="
+set "scheduler_env="
+set "notification_env="
+set "project_env="
+set "entity_management_env="
+
+pause
