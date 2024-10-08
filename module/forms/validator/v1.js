9867e3774ff6fe792a7fda9758ef2eed96b37309
saish.borkar <borkarsaish65@gmail.com>
2024-10-08 10:55:45 +0530

    new route added and validator file fix

--- module/forms/validator/v1.js
+++ module/forms/validator/v1.js
@@ -2,20 +2,12 @@ module.exports = (req) => {
 	let formsValidator = {
 		create: function () {
 			req.checkBody('type').trim().notEmpty().withMessage('type field is empty')
-			// .matches(/^[A-Za-z]+$/)
-			// .withMessage('type is invalid')
 			req.checkBody('subType').trim().notEmpty().withMessage('subType field is empty')
-			// .matches(/^[A-Za-z]+$/)
-			// .withMessage('subType is invalid')
 			req.checkBody('data').notEmpty().withMessage('data field is empty')
 		},
 		update: function () {
 			req.checkBody('type').notEmpty().withMessage('type field is empty')
-			// .matches(/^[A-Za-z]+$/)
-			// .withMessage('type is invalid')
 			req.checkBody('subType').notEmpty().withMessage('subType field is empty')
-			// .matches(/^[A-Za-z]+$/)
-			// .withMessage('subType is invalid')
 			req.checkBody('data').notEmpty().withMessage('data field is empty')
 		},
 		read: function () {
@@ -24,12 +16,7 @@ module.exports = (req) => {
 					req.checkParams('_id').notEmpty().withMessage('id param is empty')
 				} else {
 					req.checkBody('type').trim().notEmpty().withMessage('type field is empty')
-					// .matches(/^[A-Za-z]+$/)
-					// .withMessage('type is invalid')
-
 					req.checkBody('subType').trim().notEmpty().withMessage('subType field is empty')
-					// .matches(/^[A-Za-z]+$/)
-					// .withMessage('subType is invalid')
 				}
 			}
 		},
