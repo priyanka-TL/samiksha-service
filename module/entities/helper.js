641773dbb55a5678dd3046e62b3bb65485963f9c
borkarsaish65 <borkarsaish65@gmail.com>
2025-01-01 15:35:33 +0530

    self review 1

--- module/entities/helper.js
+++ module/entities/helper.js
@@ -1458,8 +1458,6 @@ module.exports = class EntitiesHelper {
 
           }
 
-          console.log(result,'result')
-
           if ( req.query.solutionId ) {
               let findQuery = {
                   _id: ObjectId(req.query.solutionId)
@@ -1488,7 +1486,6 @@ module.exports = class EntitiesHelper {
               };
                   
               let entitiesDetails = await entityManagementService.entityDocuments(filterData,entityProjections);
-              console.log(entitiesDetails,"entitiesDetails")
               if ( !entitiesDetails.success ) {
                   return resolve({
                       "message" : messageConstants.apiResponses.ENTITY_NOT_FOUND,
