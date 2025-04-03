91e639b373af41a1ec96678e755c64f24e6ec204
borkarsaish65 <borkarsaish65@gmail.com>
2025-04-03 16:28:58 +0530

    feat:savepoint

--- module/observations/helper.js
+++ module/observations/helper.js
@@ -2322,6 +2322,39 @@ module.exports = class ObservationsHelper {
   
       lastSubmissionNumber = lastSubmissionForObservationEntity.result + 1;
   
+      let programInformation = null;
+
+      if(solutionDocument.programId){
+
+      let programQueryObject = {
+        _id: solutionDocument.programId,
+        status: "active"
+      };
+
+      let programDocument = await programsHelper.list(programQueryObject, [
+         "externalId",
+         "name",
+         "description",
+         "imageCompression",
+         "isAPrivateProgram",
+       ]);
+
+       programDocument = programDocument.data.data
+       
+       if (!programDocument[0]._id) {
+         throw messageConstants.apiResponses.PROGRAM_NOT_FOUND;
+       }
+
+       programInformation =  {
+           ..._.omit(programDocument[0], [
+             "_id",
+             "components",
+             "isAPrivateProgram",
+           ]),
+         }
+
+      }
+
       let submissionDocument = {
         entityId: entityDocument._id,
         entityExternalId: entityDocument.metaInformation.externalId ? entityDocument.metaInformation.externalId : '',
@@ -2346,7 +2379,8 @@ module.exports = class ObservationsHelper {
         scoringSystem: solutionDocument.scoringSystem,
         isRubricDriven: solutionDocument.isRubricDriven,
         userProfile: observationDocument?.userProfile ?? {},
-        themes: solutionDocument.themes
+        themes: solutionDocument.themes,
+        programInformation:programInformation
       };
   
       if (solutionDocument.hasOwnProperty('criteriaLevelReport')) {
