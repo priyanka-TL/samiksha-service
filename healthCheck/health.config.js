c8e384119a8c27d91e2e75b5a6321fe441efed18
MallanagoudaB <mallanagouda@shikshalokam.org>
2025-07-15 14:28:33 +0530

    entityFix

--- healthCheck/health.config.js
+++ healthCheck/health.config.js
@@ -24,7 +24,7 @@ module.exports = {
     microservices: [
       {
         name: 'EntityManagementService',
-		url: `${process.env.INTERFACE_SERVICE_URL}/entity/health?serviceName=${process.env.SERVICE_NAME}`,
+		url: `${process.env.INTERFACE_SERVICE_URL}/entity-management/health?serviceName=${process.env.SERVICE_NAME}`,
         enabled: true,
         request: {
           method: 'GET',
